3e516379d1e2275368e12a05f2fd77384225ef59
kraxarn <me@kraxarn.com>
2020-03-25 17:38:00 +0100

    Don't define in constructor

--- src/icons.cpp
+++ src/icons.cpp
@@ -3,44 +3,40 @@
 #include <QHash>
 
 // Static definition
-QHash<QString, QString> Icons::iconNames = QHash<QString, QString>();
+QHash<QString, QString> Icons::iconNames = {
+	// File menu
+	{ "file-new",				"document-new"				},
+	{ "file-open",				"document-open"				},
+	{ "file-save-as",			"document-save-as"			},
+	{ "file-quit",				"exit"						},
+	// Edit menu
+	{ "edit-rename",				"text-field"					},
+	{ "edit-reload",				"reload"						},
+	// About menu
+	{ "about-app",				"help-about"					},
+	{ "about-qt",				"qt"							},
+	{ "about-licenses",			"license"					},
+	{ "about-update",			"download"					},
+	// Tools
+	{ "tools-move",				"object-move-symbolic"		},
+	{ "tools-link",				"draw-line"					},
+	// Other menus
+	{ "menu-edit",				"document-edit"				},
+	{ "menu-delete",				"delete"						},
+	// Text formatting
+	{ "format-bold",				"format-text-bold"			},
+	{ "format-italic",			"format-text-italic"			},
+	{ "format-underline",		"format-text-underline"		},
+	{ "format-strikethrough",	"format-text-strikethrough"	},
+	// Validation engine
+	{ "validate-ok",				"emblem-checked"				},
+	{ "validate-fail",			"emblem-error"				},
+	{ "validate-disabled",		"emblem-pause"				},
+	{ "validate-none",			"emblem-question"			},
+};
 
 Icons::Icons()
 {
-	if (iconNames.isEmpty())
-	{
-		iconNames = {
-			// File menu
-			{ "file-new",				"document-new"				},
-			{ "file-open",				"document-open"				},
-			{ "file-save-as",			"document-save-as"			},
-			{ "file-quit",				"exit"						},
-			// Edit menu
-			{ "edit-rename",				"text-field"					},
-			{ "edit-reload",				"reload"						},
-			// About menu
-			{ "about-app",				"help-about"					},
-			{ "about-qt",				"qt"							},
-			{ "about-licenses",			"license"					},
-			{ "about-update",			"download"					},
-			// Tools
-			{ "tools-move",				"object-move-symbolic"		},
-			{ "tools-link",				"draw-line"					},
-			// Other menus
-			{ "menu-edit",				"document-edit"				},
-			{ "menu-delete",				"delete"						},
-			// Text formatting
-			{ "format-bold",				"format-text-bold"			},
-			{ "format-italic",			"format-text-italic"			},
-			{ "format-underline",		"format-text-underline"		},
-			{ "format-strikethrough",	"format-text-strikethrough"	},
-			// Validation engine
-			{ "validate-ok",				"emblem-checked"				},
-			{ "validate-fail",			"emblem-error"				},
-			{ "validate-disabled",		"emblem-pause"				},
-			{ "validate-none",			"emblem-question"			},
-		};
-	}
 }
 
 QIcon Icons::get(const QString &name)
