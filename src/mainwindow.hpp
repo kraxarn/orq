6c648334045ff8955024a348df02c9c5588552c9
kraxarn <me@kraxarn.com>
2020-03-25 17:45:53 +0100

    Clean up

--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -7,21 +7,17 @@ class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
-private: 
-	QMenuBar* _menubar;
-	QMenu* 	  _fileMenu;
-	QAction*  _fileNew;
-	QAction*  _fileOpen;
-	QAction*  _fileSaveAs;
-	QAction*  _fileQuit;
-
 public:
 	MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 
-	void AddMenuBar();
-	void NewProject();
-	void OpenProject();
-	void SaveProject();
-	void QuitProject();
+private: 
+	QMenuBar	*menubar;
+	QMenu		*fileMenu;
+	QAction		*fileNew;
+	QAction		*fileOpen;
+	QAction		*fileSaveAs;
+	QAction		*fileQuit;
+
+	void addMenuBar();
 };
