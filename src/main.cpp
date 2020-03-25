0fd4102da85ddac5566245295bd66e2c6c90c6f8
kraxarn <me@kraxarn.com>
2020-03-25 18:13:38 +0100

    Do more stuff in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include "mainwindow.hpp"
 
 #include <QApplication>
+#include <QCoreApplication>
 
+// This should be set from CMake later
 #define VERSION "v1.90.0"
 
 int main(int argc, char *argv[])
 {
+	// Setup application variables
+	QCoreApplication::setOrganizationName("orqapp");
+	QCoreApplication::setApplicationName("orq");
+
+	// Create main window and app
 	QApplication app(argc, argv);
 	MainWindow window;
+
+	// Show main window and run main Qt loop
 	window.show();
 	return app.exec();
 }
